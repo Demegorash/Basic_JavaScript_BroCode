c9c032b76a74c7d1903f6e1d8fa2a0272b0c450d
Demegorash <felipe.solisgranados@gmail.com>
2024-05-22 18:19:01 -0600

    Day 1 learning JavaScript thanks to BroCode

---
+++ java_from_zero/js_basic_website.js
@@ -0,0 +1,18 @@
+console.log("Hello People");
+console.log("I like Pizza!!!");
+
+// Remove the comment in order to see a window alert at browser when refresh on it.
+
+// window.alert("This is and alert window!!!");
+// window.alert("I like pizza a lot!!!");
+
+// This is a JavaScript comment!
+
+/*
+This is a comment
+in multiple 
+lines in Java Script
+ */
+
+document.getElementById("myH1").textContent = "Hello!!!"
+document.getElementById("myP").textContent = "I like pizza a lot!!!"
