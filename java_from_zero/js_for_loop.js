bb5aaa5abf4e40cdcc7b31fce11af8f84cef08aa
Demegorash <felipe.solisgranados@gmail.com>
2024-06-02 19:05:45 -0600

    More exercises and theory to this project.  Project 3 Guessing Game in JS!

---
+++ java_from_zero/js_for_loop.js
@@ -0,0 +1,45 @@
+// a for loop repeat some code a limited amount of time
+
+/*
+for(let i = 0; i <= 2; i++) {
+    console.log("Hello");
+}
+
+for(let i = 2; i < 10; i+=2) {
+    console.log(i);
+}
+
+for(let i = 1; i < 10; i+=2) {
+    console.log(i);
+}
+*/
+
+for(let i = 10; i > 0; i--) {
+    console.log(i);
+}
+
+console.log("Happy New Year!!!!!");
+
+// continue
+
+for(let i = 1; i <=20; i++) {
+
+    if(i == 13) {
+        continue;
+    }
+    else {
+        console.log(i); 
+    }
+}
+
+// break
+
+for(let i = 1; i <=20; i++) {
+    
+    if(i == 13) {
+        break;
+    }
+    else {
+        console.log(i); 
+    }
+}
